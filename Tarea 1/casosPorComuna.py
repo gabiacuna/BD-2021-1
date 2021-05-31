66e09db0f6d8edea4fffdd98459116e0e5e8b7b3
gabiacuna <gabriela.acuna@usm.cl>
2021-05-31 12:29:43 -0400

    claves foraneas

--- Tarea 1/casosPorComuna.py
+++ Tarea 1/casosPorComuna.py
@@ -11,7 +11,9 @@ cursor.execute(
             Codigo_comuna INTEGER NOT NULL,
             Poblacion INTEGER NOT NULL,
             Casos_confirmados INTEGER NOT NULL,
-            PRIMARY KEY (Codigo_comuna)
+            Codigo_region INTREGER NULL REFERENCES CASOS_POR_REGION(Codigo_region),
+            PRIMARY KEY (Codigo_comuna),
+            FOREGIN KEY (Codigo_region)
         )
     """
 )
@@ -28,7 +30,7 @@ with open('CasosConfirmadosPorComuna.csv') as casosPorC_file:
 
 try:
     for comuna,id_comuna, pob, casos in casosPorC:
-        cursor.execute (
+        cursor.execute (    #Revidar como queda el cod_region
             """INSERT INTO CASOS_POR_COMUNA VALUES (:Comuna, :Codigo_comuna, :Poblacion, :Casos_confirmados)""",[comuna,int(id_comuna), int(pob), int(casos)]
         )
 except Exception as err:
@@ -37,4 +39,26 @@ else:
     print('Insercion completada')
     connection.commit()
 
+
+with open('RegionesComunas.csv') as regionesC_file:
+
+    next(regionesC_file) #Para saltarse la primera linea con headers
+
+    for line in regionesC_file:     #Region,Codigo Region,Codigo Comuna
+        line = line.strip().split(',')
+        region,id_region, id_comuna = line
+        id_region, id_comuna = int(id_region), int(id_comuna)
+
+        #### Hacer el update para agregar el cod_region a todas las comunas!!!!!
+        # try:
+        #     cursor.execute (
+        #         """INSERT INTO COMUNAS_POR_REGION VALUES (:Codigo_region, :Region, :Codigo_comuna)""",[id_region,region, id_comuna ]
+        #     )
+        # except Exception as err:
+        #     print('Hubo algun error insertando datos:',err)
+        # else:
+        #     print('Insercion completada')
+        #     connection.commit()
+
+connection.close()
 connection.close()
